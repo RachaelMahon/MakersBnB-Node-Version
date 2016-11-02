7a04d08c3aafed9123d9900deaa053790a132c91
RachaelMahon <rachmahon@gmail.com>
2016-11-02 15:05:45 +0000

    JQuery working - can't figure out how to pull models in to the controller but can get it into the html document using the script tag right now

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+// var model = require('./models/addresses.js');
+// var prop = new Properties();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
+//   sequelize.query('SELECT * FROM addresses').then(function(addresses) {
+//     console.log(addresses);
+//   });
   res.render('index', { title: 'Express' });
 });
 
