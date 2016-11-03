c4b50c7c117609ace4bcfb22668b46ca42620eb0
Tom Page <tom.w.page@gmail.com>
2016-11-03 11:57:00 +0000

    Jasmine testing

---
+++ spec/index_spec.js
@@ -0,0 +1,21 @@
+require ("../bin/www")
+var request = require("request");
+//var helloWorld = require('../routes/index.js')
+var base_url = "http://localhost:3000"
+
+
+
+describe("Makers BNB server test", function() {
+  describe("GET /", function(){
+    console.log("Started2!")
+    it("returns statues code 200", function(done){
+      request.get("http://localhost:3000", function(error, response, body) {
+        console.log("Started3!")
+        //console.log(response)
+        expect(response.statusCode).toBe(200);
+
+        done();
+      });
+    });
+});
+});
